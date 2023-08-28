7821734ef5e055d526624bca16100ccc2f613ef8
Piyush Aggarwal <piyushaggarwal002@gmail.com>
2023-08-29 03:21:24 +0530

    fix cmake typo

--- src/plugins/Macros.cmake
+++ src/plugins/Macros.cmake
@@ -7,9 +7,10 @@ function(add_plugin plugin)
   cmake_parse_arguments(PLUGIN "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN}) 
   # prefix is set to PLUGIN, so args will be parsed as PLUGIN_args 
   add_library(${plugin} MODULE ${PLUGIN_SOURCES})
+  target_link_libraries(${plugin} sortverse-core)
   target_include_directories(${plugin} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
-  set_target_properties(${plugin} PROPERTIES
-  target_link_libraries(${plugin} STATIC sortverse-core)
-  LIBRARY_OUTPUT_DIRECTORY "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/plugins")
-
+  set_target_properties(${plugin}
+    PROPERTIES
+      LIBRARY_OUTPUT_DIRECTORY "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/plugins"
+    )
 endfunction(add_plugin)
